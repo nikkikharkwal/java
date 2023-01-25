804d5437a5ec6d3c63def46cb452e35dda8b40b7
nikkikharkwal <nikki.kharkwal@gmail.com>
2023-01-25 15:12:35 +0530

    Square and Sum

---
+++ Basic/Square and Sum.java
@@ -0,0 +1,42 @@
+/*
+Description
+
+You are given three numbers, stored in variables with the following names
+one, two, three
+You have to find the square of the three numbers, and find the sum of their square values
+Note : Square of a number, is a number multiplied by itself. For example, the square of 3, is 3 * 3 = 9
+
+
+Input
+The first and the only line of the input contains, the values stored inone, two, three
+
+Output
+Print the sum of the squares of the values stored inone, two, three
+
+Sample Input 1 
+
+1 2 3
+Sample Output 1
+
+14
+Hint
+
+In the sample test case, the value stored inone = 1, two = 2, three = 3.
+
+The square of the values stored will be
+
+one = 1 * 1 = 1
+
+two = 2 * 2 = 4
+
+three = 3 * 3 = 9
+
+The resultant sum becomesone + two + three = 1 + 4 + 9 = 14, which is the required answer.
+  */
+
+public static void squareAndSum(int one,int two,int three){
+        //write your code here
+        int ans;
+        ans = (one * one) + (two * two) + (three * three);
+        System.out.println(ans);
+    }
