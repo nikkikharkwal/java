3445b66ec8fcff0d021ea43ca6207bdf4d5a5387
nikkikharkwal <nikki.kharkwal@gmail.com>
2023-01-25 15:00:13 +0530

    Multiply by 50

---
+++ Basic/Multiply by 50.java
@@ -0,0 +1,35 @@
+/*
+Description
+
+You are given a number stored in a variable, with the namenumber
+
+Multiply the number stored in the variablenumberby 50, and print the result obtained
+
+
+Input
+The first and the only line of the input contains the number stored in the variablenumber
+
+Constraints
+
+1 <= N <= 20
+
+
+Output
+Print a single number, the result obtained by multiplying it by 50
+
+
+Sample Input 1 
+
+2
+Sample Output 1
+
+100
+Hint
+
+In the sample test case, the value stored in the variablenumber
+  */
+
+public static void multiplyBy50(int number){
+    //write your code here
+    System.out.println(number * 50);
+}
