479485b9246adc16e12f1bdf2d51763123b04918
nikkikharkwal <nikki.kharkwal@gmail.com>
2023-01-25 15:09:47 +0530

    Cube of a Number

---
+++ Basic/Cube of a number.java
@@ -0,0 +1,34 @@
+/*
+Description
+
+Given a number N, find its cube.
+
+
+Input
+The first and the only line of the input contains the number N.
+
+Constraints
+
+1 <= N <= 30
+
+
+Output
+Print the cube of a number.
+
+
+Sample Input 1 
+
+2
+Sample Output 1
+
+8
+*/
+
+public static void cubeOfNumber(int N){
+        //write your code here
+        int cube;
+        cube = N*N*N;
+        System.out.println(cube);
+    }
+
+
