fe98ae7467710b5f6715fce4715292d97283986d
nikkikharkwal <nikki.kharkwal@gmail.com>
2023-02-03 09:35:05 +0530

    gitignore

---
+++ Variables and Datatypes/DataType.java
@@ -0,0 +1,14 @@
+public class DataType{
+	public static void main(String args[]){
+		byte b = 8;
+		System.out.println(b);
+		char ch = 'a';
+		System.out.println(ch);
+		boolean var = true; //false
+		float price = 10.5f;
+		int number = 25;
+		//long
+		//double
+		short n = 240;
+	}
+}
---
+++ Variables and Datatypes/Sum.java
@@ -0,0 +1,9 @@
+public class Sum{
+	public static void main(String args[]) {
+		int a = 10;
+		int b = 5;
+
+		int sum = a + b;
+		 System.out.println(sum);
+	}
+}
